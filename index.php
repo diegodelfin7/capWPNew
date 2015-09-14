a59948bf5d78cd108953000bd33884454df017f5
jose antonio <fock.kiss@gmail.com>
2015-09-14 16:54:51 -0500

    Primer commit Inicial Industrail

---
+++ index.php
@@ -0,0 +1,277 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>app cat</title>
+	<link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap.min.css" />
+	<link rel="stylesheet" href="bower_components/components-font-awesome/css/font-awesome.min.css" />
+	<link rel="stylesheet" href="public/css/app.css" />
+</head>
+<body>
+	<div id="landing-video" class=" text-center">
+		<div class="title-landing-video">TECNOLOGÍA</div>
+		<div class="description-landing-video">LA INNOVACIÓN ESTA EN NUESTRA SANGRE</div>
+		<a data-toggle="modal" class="btn-landing-playvideo" data-target="#ModalYTCat"><img src="public/img/boton-play.png" height="112" width="112"></a>
+	</div>
+	<div id="landing-blockthumb">
+		<div class="container text-center inner-blockthumb">
+			<div class="col-md-3 col-lg-3">
+				<div class="block-thumb">
+					<div class="title-block-thumb">SRX</div>
+					<p class="description-block-thumb">LA MEJOR RESISTENCIA ANTIDELIZANTE</p>
+					<img src="public/img/srx-thumb.png" class="block-img-thumb" />
+				</div>
+			</div>
+			<div class="col-md-3 col-lg-3">
+				<div class="block-thumb">
+					<div class="title-block-thumb">ITECHNOLOGY</div>
+					<p class="description-block-thumb">COMBINACIÓN DE DURABILIDAD Y CONFORT</p>
+					<img src="public/img/ithe-thumb.png" class="block-img-thumb" />
+				</div>
+			</div>
+			<div class="col-md-3 col-lg-3">
+				<div class="block-thumb">
+					<div class="title-block-thumb">ERGO</div>
+					<p class="description-block-thumb">DISEÑO ERGONÓMICO <br /> CON MAYOR FLEXIBILIDAD</p>
+					<img src="public/img/ergo-thumb.png" class="block-img-thumb" />
+				</div>
+			</div>
+			<div class="col-md-3 col-lg-3">
+				<div class="block-thumb">
+					<div class="title-block-thumb">ERGO LITE</div>
+					<p class="description-block-thumb">DISEÑO ERGONÓMICO <br /> DE ERGO MUCHO MÁS LIGERO</p>
+					<img src="public/img/ergolite-thumb.png" class="block-img-thumb" />
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="srx contZapt">
+		<figure>
+			<img src="public/img/srx.png" alt="">			
+		</figure>
+		<div class="srx-title title">SRX</div>
+
+	</div>
+	
+	<div class="srx-more">
+		<div class="container text-center">
+			<div class="col-md-4">
+				<div class="block-srx-more">
+					<img src="public/img/srx-1.png"  width="213" height="163" alt="" />
+					<div class="src-more-title">DYNAMO 6'' CT</div>
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="block-srx-more">
+					<img src="public/img/srx-2.png"  width="213" height="163" alt="" />
+					<div class="src-more-title">DAWSON 6'' ct</div>
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="block-srx-more">
+					<img src="public/img/srx-3.png"  width="213" height="163" alt="" />
+					<div class="src-more-title">SENSOR HI ST</div>
+				</div>
+			</div>
+		</div>
+		<div class="containerbtn text-center">
+			<a href="#" class="btnhere-src-more">DESCUBRE MÁS AQUÍ</a>
+		</div>
+	</div>
+
+	<div class="itech contZapt">
+		<figure>
+			<img src="public/img/itech.png" alt="">			
+		</figure>		
+		<div class="itech-title title">ITECHNOLOGY</div>
+	</div>
+	<div class="itech-more">
+		<div class="container text-center">
+			<div class="col-md-4">
+				<div class="block-itech-more">
+					<img src="public/img/itech-1.png"  width="213" height="163" alt="" />
+					<div class="itech-more-title">FUSELAGE HI CT</div>
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="block-itech-more">
+					<img src="public/img/itech-2.png"  width="213" height="163" alt="" />
+					<div class="itech-more-title">FREEDOM ST</div>
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="block-itech-more">
+					<img src="public/img/itech-3.png"  width="213" height="163" alt="" />
+					<div class="itech-more-title">CARLIE ST</div>
+				</div>
+			</div>
+		</div>
+		<div class="containerbtn text-center">
+			<a href="#" class="btnhere-itech-more">DESCUBRE MÁS AQUÍ</a>
+		</div>
+	</div>
+
+	<div class="ergo contZapt">
+		<figure>
+			<img src="public/img/ergo.png" alt="">			
+		</figure>		
+		<div class="ergo-title title">ERGO</div>
+	</div>
+	<div class="ergo-more">
+		<div class="container text-center">
+			<div class="col-md-4">
+				<div class="block-ergo-more">
+					<img src="public/img/ergo-1.png"  width="213" height="163" alt="" />
+					<div class="ergo-more-title">ARGON CT</div>
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="block-ergo-more">
+					<img src="public/img/ergo-2.png"  width="213" height="163" alt="" />
+					<div class="ergo-more-title">COOLANT ST</div>
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="block-ergo-more">
+					<img src="public/img/ergo-3.png"  width="213" height="163" alt="" />
+					<div class="ergo-more-title">TERBIUM CT</div>
+				</div>
+			</div>
+		</div>
+		<div class="containerbtn text-center">
+			<a href="#" class="btnhere-ergo-more">DESCUBRE MÁS AQUÍ</a>
+		</div>
+	</div>
+
+	<div class="ergo-lite  contZapt">
+		<figure>
+			<img src="public/img/ergolite.png" alt="">			
+		</figure>		
+		<div class="ergo-lite-title title">ERGO LITE</div>
+		
+	</div>
+	<div class="ergo-lite-more">
+		<div class="container text-center">
+			<div class="col-md-4">
+				<div class="block-ergo-lite-more">
+					<img src="public/img/ergolite-1.png"  width="213" height="163" alt="" />
+					<div class="ergo-lite-more-title">STREAMLINE CT</div>
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="block-ergo-lite-more">
+					<img src="public/img/ergolite-2.png"  width="213" height="163" alt="" />
+					<div class="ergo-lite-more-title">INFRASTRUCTURE ST</div>
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="block-ergo-lite-more">
+					<img src="public/img/ergolite-3.png"  width="213" height="163" alt="" />
+					<div class="ergo-lite-more-title">SWITCH CT</div>
+				</div>
+			</div>
+		</div>
+		<div class="containerbtn text-center">
+			<a href="#" class="btnhere-ergo-lite-more">DESCUBRE MÁS AQUÍ</a>
+		</div>
+	</div>
+
+	<div class="feat-tech">
+		<div class="title-feat-tech">
+			FEATURES Y TECNOLOGÍAS
+		</div>
+		<div class="container text-center">
+			<div class="row">
+				<div class="col-md-4">
+					<div class="item-feat-tech">
+						<div class="itemtit-feat-tech">Punta COMPUESTA</div>
+						<img src="public/img/feature1.jpg" alt="" />
+						<div class="itemdes-feat-tech">PUNTA NO METÁLICA INDETECTABLE EN LUGARES DE SEGURIDAD ELECTRÓNICA</div>
+					</div>
+				</div>
+				<div class="col-md-4">
+					<div class="item-feat-tech">
+						<div class="itemtit-feat-tech">PUNTA DE ACERO</div>
+						<img src="public/img/feature2.jpg" alt="" />
+						<div class="itemdes-feat-tech">PUNTA ULTRA RESISTENTE CONTRA IMPACTOS Y COMPRESIONES</div>
+					</div>
+				</div>
+				<div class="col-md-4">
+					<div class="item-feat-tech">
+						<div class="itemtit-feat-tech">RESISTENCIA AL GUA</div>
+						<img src="public/img/feature3.jpg" alt="" />
+						<div class="itemdes-feat-tech">MANTIENE LA HUMEDAD FUERA CON CUEROS Y COSTURAS IMPERMEABLES</div>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-4">
+					<div class="item-feat-tech">
+						<div class="itemtit-feat-tech">RIESGO ELÉCTRICO</div>
+						<img src="public/img/feature4.jpg" alt="" />
+						<div class="itemdes-feat-tech">PROTECCIÓN CONTRA CIRCUITOS ABIERTOS HASTA 18.000 VOLTIOS</div>
+					</div>
+				</div>
+				<div class="col-md-4">
+					<div class="item-feat-tech">
+						<div class="itemtit-feat-tech">ANTIDESLIZANTE</div>
+						<img src="public/img/feature5.jpg" alt="" />
+						<div class="itemdes-feat-tech">SUELA CON REDUCCIÓN DE REISGO ANTE CAÍDAS Y RESBALONES</div>
+					</div>
+				</div>
+				<div class="col-md-4">
+					<div class="item-feat-tech">
+						<div class="itemtit-feat-tech">DISIPACIÓN ESTÁTICA</div>
+						<img src="public/img/feature6.jpg" alt="" />
+						<div class="itemdes-feat-tech">REDUCEN AL MINIMO LA ACUMULACIÓN DE ELECTRICIDAD ESTÁTICA</div>
+					</div>
+				</div>	
+			</div>
+		</div>
+	</div>
+	<div class="form-contact">
+		<div class="container">
+			<div class="text-center title-form-contact">CONTÁCTANOS</div>
+			<form>
+				<div class="form-group">
+					<input type="text" placeholder="NOMBRE" class="form-control css-placeholder" id="nameFormContact" />
+				</div>
+				<div class="form-group">
+					<input type="email" placeholder="MAIL" class="form-control css-placeholder" />
+				</div>
+				<div class="form-group">
+					<textarea rows="9" cols="50" placeholder="DESCRIPCIÓN"  class="form-control css-placeholder" ></textarea>
+				</div>
+				<div class="form-group">
+	   				<div class="text-center">
+	        			<button type="submit" class="btnsubmit-form">Enviar</button>
+	    			</div>
+				</div>
+			</form>
+		</div>
+	</div>
+	<script src="bower_components/jquery/dist/jquery.min.js"></script>
+	<script src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+
+	<!--  Modal You Tube-->
+	<div class="modal fade" id="ModalYTCat" aria-hidden="true">
+		<div class="modal-dialog">
+        			<div class="modal-content">
+            			<div class="modal-header">
+                				<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                 				<h4 class="modal-title">Modal title</h4>
+				</div>
+            			<div class="modal-body"><div class="te"></div></div>
+            			<div class="modal-footer">
+                				<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                				<button type="button" class="btn btn-primary">Save changes</button>
+            			</div>
+        			</div>
+        		<!-- /.modal-content -->
+    		</div>
+    	<!-- /.modal-dialog -->
+	</div>
+</body>
+</html>
